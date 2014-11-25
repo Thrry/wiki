64ec4c406dc79a1078451eec9a2d79dad39fd36e
Thrry <thierryjouannic@gmail.com>
2014-11-25 14:44:07 +0100

    début

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "faker"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def fakename
+  name = Faker::Hacker.adjective.capitalize + " " + Faker::Hacker.noun + " "
+  return name + Faker::Hacker.verb + " " + Faker::Hacker.noun + "."
+end
+
+10.times do
+  puts "coucou"
+    Article.create(
+      title: fakename,
+      content: Faker::Lorem.paragraph(2),
+  )
+end
